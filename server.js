ea436d4ea6bb0cd4251d096c06aa2fea832065ce
vaisanenjaska <vaisanenjaska@gmail.com>
2017-04-05 18:00:49 +0300

    Commit

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"})
-  response.write("hey")
-  response.end()
+app.get('/', function(req, res) {
+  res.send("Hey")
 })
