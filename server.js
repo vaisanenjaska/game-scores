6b1842521243b071cb1aed529791fdd65b5eb421
vaisanenjaska <vaisanenjaska@gmail.com>
2017-04-26 22:18:34 +0300

    Added routes for adding games, scores, and getting both of them plus the front-end for them

--- server.js
+++ server.js
@@ -2,28 +2,87 @@ var express = require('express')
 var app = express()
 var path = require("path")
 var pg = require('pg')
+var bodyParser = require('body-parser')
 
 function DBConnect(res) {
   pg.defaults.ssl = true;
   let response = {}
   pg.connect(process.env.DATABASE_URL, function(err, client) {
     if (err) throw err
-    console.log('Connected to postgres! Getting schemas...')
     let i = 0
     client.query('SELECT * from games;').on('row', function(row) {
-        response[i++] = row.name
+        response[i++] = row
     }).then( function() {
       res.send(response)
     })
   })
 }
 
-app.get('/', function (req, res) {
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
   res.sendFile(__dirname + "/index.html")
 })
 
-app.post('/', function (req,res) {
+app.post('/', function(req,res) {
   DBConnect(res)
+})
+
+app.post('/scores/:id', function(req, res) {
+  let id = req.params.id
+  pg.defaults.ssl = true;
+  let response = {}
+  pg.connect(process.env.DATABASE_URL, function(err, client) {
+    if (err) throw err
+    let i = 0
+    client.query('SELECT * from scores where gameid=$1;', [id]).on('row', function(row) {
+        response[i++] = row
+    }).then( function() {
+      res.send(response)
+    })
+  })
+})
+
+app.post('/submitscore/:id', function(req, res) {
+  let id = req.params.id
+  pg.defaults.ssl = true;
+  let response = {}
+  pg.connect(process.env.DATABASE_URL, function(err, client) {
+    if (err) throw err
+    let i = 0
+    client.query('INSERT INTO scores ("gameid", "nick", "score") VALUES ($1, $2, $3);', [id, req.body.name, req.body.score])
+  })
+  res.send(req.body.name)
+})
+
+app.get('/games/:id', function(req, res) {
+  let id = req.params.id
+  pg.defaults.ssl = true;
+  let response = {}
+  pg.connect(process.env.DATABASE_URL, function(err, client) {
+    if (err) throw err
+    let i = 0
+    client.query('SELECT * from scores where gameid=$1;', [id]).on('row', function(row) {
+        response = row
+    }).then( function() {
+      res.send(response)
+    })
+  })
+})
+
+app.post('/game', function (req,res) {
+  pg.defaults.ssl = true;
+  let response = {}
+  pg.connect(process.env.DATABASE_URL, function(err, client) {
+    if (err) throw err
+    let i = 0
+    client.query('INSERT INTO games ("name") VALUES ($1);', [req.body.name])
+  })
+  res.send(req.body.name)
 })
 
 app.listen(process.env.PORT || 3000, function () {
